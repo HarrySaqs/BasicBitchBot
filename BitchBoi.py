65df7512be49c70722c776308cf06b534cae787c
HarrySaqs <nstackpolewow@gmail.com>
2016-04-13 17:56:43 -0400

    Update BitchBoi.py

--- BitchBoi.py
+++ BitchBoi.py
@@ -10,7 +10,7 @@ from random import randint
 
 #This is the first rough draft code for the stupid bot I have made.
 
-TOKEN = 'Your Token Here' #It would be better to take this as an arguement. 
+TOKEN = 'Your Token Here' #It would be better to take this as an arguement when you call the script, rather than hardcoding it in. 
 
 bot = telepot.Bot(TOKEN)
 
@@ -21,41 +21,43 @@ def handle(msg):
 	Tupac = open('2pac.jpg', 'rb')
 	
 	pprint.pprint(msg)
-	chatid = msg['chat']['id']
+	
+	#lets me know if the message is a pciture or sticker or text, etc. whether the chat is a group or private or channel
+	#and gives me the id of teh chat so the bot can respond.
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	
 	if(content_type == 'text'):
 		
-		text = msg['text'].lower()
+		text = msg['text'].lower() #puts the message in lowercase so I don't have to test for uppercase versions of words.
 		
 		if(text == '/memes'):
 			x = random.randint(1,138) #there are 138 pictures in the folder,named 1.png to 138.png that it chooses from at random.
 			pic = str(x) + '.png'
 			y = open(pic, 'rb')
-			bot.sendPhoto(chatid, y)
+			bot.sendPhoto(chat_id, y)
 			
 			
 		elif (text.find('weed') != -1 or text.find('420') != -1 or text == '/weed'):
 			bot.sendMessage(chatid, " Get on the roof, let's get smoked out and blaze with me.")
-			bot.sendPhoto(chatid, Tupac)
+			bot.sendPhoto(chat_id, Tupac)
 			
 		elif (text.find('harry') != -1 and (text.find('stupid') != -1 or text.find('dumb') != -1 or text.find('fucker') != -1)):
-			bot.sendMessage(chatid, "Why don't you make like a tree and fuck off, fucker!")
+			bot.sendMessage(chat_id, "Why don't you make like a tree and fuck off, fucker!")
 			
 		elif (text.find('vape') != -1):
-			bot.sendSticker(chatid, 'BQADBAADbgADqpW-A-5eRYGeubOIAg')
+			bot.sendSticker(chat_id, 'BQADBAADbgADqpW-A-5eRYGeubOIAg')
 			
 		elif (text == '/suggest'):
-			bot.sendMessage(chatid, 'https://github.com/HarrySaqs/BasicBitchBot/issues')
+			bot.sendMessage(chat_id, 'https://github.com/HarrySaqs/BasicBitchBot/issues')
 			
 		elif (text == '/tits'):
 			y = open('CornPorn.png', 'rb')
-			bot.sendPhoto(chatid, y)
+			bot.sendPhoto(chat_id, y)
 			
 			
 #	elif(content_type == 'sticker'):
-	#	bot.sendSticker(chatid, msg['sticker']['file_id'])
-	#	bot.sendMessage(chatid, 'nice sticker nerd')
+	#	bot.sendSticker(chat_id, msg['sticker']['file_id'])
+	#	bot.sendMessage(chat_id, 'nice sticker nerd')
 		
 	
 	
